3bd8a1eaa143b423c2dd83f6ca7340e527688097
fcester <101715799+fcester@users.noreply.github.com>
2025-05-21 09:33:07 +0200

    Update Actual_Stock.py

--- Actual_Stock.py
+++ Actual_Stock.py
@@ -6,6 +6,8 @@ import os
 # Leer tickers desde CSV
 tickers_df = pd.read_csv("Tickers.csv")
 tickers = tickers_df["ticker"].dropna().tolist()
+tickers_dict = tickers_df["ticker"].dropna().todict()
+
 
 # Descargar datos últimos 5 días
 df = yf.download(tickers, period="5d", interval="1d", auto_adjust=True)['Close']
@@ -13,8 +15,29 @@ df = yf.download(tickers, period="5d", interval="1d", auto_adjust=True)['Close']
 # Tomar solo la última fila disponible (último cierre)
 df = df.tail(1)
 
+---
+for ticker_look in tickers_dict:
+    ticker_yf = yf.Ticker(ticker_look)
+    temp = ticker_yf.info
+    
+    # Lista de atributos que te interesan
+    attributes_of_interest = [
+        "marketCap", "trailingPE", 
+        "forwardPE", "beta", "trailingEps"
+    ]
+    
+    # Crear un diccionario con solo los atributos deseados
+    filtered_data = {attr: temp.get(attr) for attr in attributes_of_interest}
+    
+    ticker_info = pd.DataFrame([filtered_data]) # Crear un DataFrame directamente
+    dow_stats[ticker_look] = ticker_info
+
+all_stats_info = pd.concat(dow_stats, keys=dow_stats.keys(), names=['ticker', 'Index'])
+---
 # Archivo destino
+file_info_name = "Stock_Info.csv"
 file_name = "Actual_Stock.csv"
+all_stats_info.to_csv(file_info_name)
 
 if os.path.exists(file_name) and os.path.getsize(file_name) > 0:
     df_existente = pd.read_csv(file_name, index_col=0)
