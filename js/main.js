4f30211b380b71caa453cc8deff6cde80dd4dc73
fcester <101715799+fcester@users.noreply.github.com>
2026-07-23 19:04:38 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,6 +5,8 @@ import { inicializarHome } from './ui-home.js';
 async function iniciar() {
   try {
     const datos = await cargarTodosLosDatos();
+    console.log('DATOS RECIBIDOS:', datos);
+    console.log('Cantidad de tickers en screener:', datos.screener ? datos.screener.length : 'screener es undefined');
     inicializarHome(datos);
 
     document.getElementById('btn-ver-todo').addEventListener('click', () => {
